d0bf5b5424211252493b9c76d69528e718092d7b
Google61 <golubev634@gmail.com>
2022-12-25 17:12:23 +0300

    Update setup-tunnel.sh

--- setup-tunnel.sh
+++ setup-tunnel.sh
@@ -9,12 +9,15 @@ unzip ngrok-stable-linux-amd64.zip
 chmod +x ngrok
 ./ngrok authtoken $ngrokauthtoken
 ./ngrok tcp --region=$ngrokregion 25565 &
+./ngrok http --region=$ngrokregion 80 &
 sleep 2s
 url=$(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url | sed 's#tcp://##g')
 echo "url=$url">>./settings.cfg
 echo "ip=$(dig +short $(echo "$url" | cut -f1 -d":"))">>./settings.cfg
 echo "port=$(echo $url | grep -o -E '[^:]+$')">>./settings.cfg
 echo "ngrok URL: $url"
+rfw=$(curl -s localhost:4040/api/tunnels | jq -r .tunnels[1].public_url )
+echo "rfwadmin URL: $rfw"
 ;;
 playit)
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | sudo apt-key add -
